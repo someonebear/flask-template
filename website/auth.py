9624722f2f365b5151e40280a4ff1ca38d818f42
Zhihao <zhi.lin1@outlook.com>
2022-04-23 17:29:14 +0800

    Add field checking. Add error message flash

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
@@ -13,10 +13,11 @@ def logout():
 @auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
   if request.method == 'POST':
-    email = request.form.get("form")
+    email = request.form.get("email")
     firstName = request.form.get("firstName")
     password1 = request.form.get("password1")
     password2 = request.form.get("password2")
+    fields = [email, firstName, password1, password2]
 
     if len(email) < 4:
       flash("Email must be greater than 3 characters.", category="error")
@@ -28,5 +29,7 @@ def sign_up():
       flash("Password must be longer than 6 characters.", category="error")
     else:
       flash("Account created.", category="success")
+
+    
   return render_template("sign_up.html")
   
