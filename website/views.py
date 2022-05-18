cd5853848e2c8235cf79ea797699f7db8cddd5f7
Zhihao <zhi.lin1@outlook.com>
2022-05-18 17:08:29 +0800

    Add delete not functionality

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, flash, request
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+import json, Jsonify
 
 views = Blueprint("views", __name__)
 
@@ -20,3 +21,16 @@ def home():
       flash('Note added!', category='success')
 
   return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+  note = json.loads(request.data)
+  noteId = note['noteId']
+  note = Note.query.get(noteId)
+  if note:
+    if note.user_id == current_user.id:
+      db.session.delete(note)
+      db.session.commit()
+  
+  return Jsonify({})
