99232c5da867f58d5356a4a2383725afde86b9ab
Zhihao <zhi.lin1@outlook.com>
2022-05-18 16:06:46 +0800

    Add create user functionality and password hash

--- website/__init__.py
+++ website/__init__.py
@@ -18,13 +18,13 @@ def create_app():
   app.register_blueprint(views, url_prefix="/")
   app.register_blueprint(auth, url_prefix="/")
   
-  from .models import models
+  from .models import User, Note
 
   create_database(app)
 
   return app
 
 def create_database(app):
-  if not path('website/' + DB_NAME):
+  if not path.exists('website/' + DB_NAME):
     db.create_all(app=app)
     print("Created Database!")
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
 
@@ -28,8 +30,11 @@ def sign_up():
     elif len(password1) < 7:
       flash("Password must be longer than 6 characters.", category="error")
     else:
+      new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+      db.session.add(new_user)
+      db.session.commit()
       flash("Account created.", category="success")
-
+      return redirect(url_for('views.home'))
     
   return render_template("sign_up.html")
   
