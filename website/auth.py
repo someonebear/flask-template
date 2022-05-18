deb9307d27ba3413ce07164b8dfcb1802c490d13
Zhihao <zhi.lin1@outlook.com>
2022-05-18 16:16:24 +0800

    Add check password/email functionality.

--- website/auth.py
+++ website/auth.py
@@ -7,7 +7,17 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+  if request.method == 'POST':
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+      if check_password_hash(user.password, password):
+        flash('Logged in successfully!', category='success')
+
   return render_template("login.html")
+
 
 @auth.route("/logout")
 def logout():
