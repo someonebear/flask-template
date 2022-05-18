14d2fae30cdf5b38fdaae6ea82cf8f8328c0bde7
Zhihao <zhi.lin1@outlook.com>
2022-05-18 16:48:33 +0800

    Add import db

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
 from .models import Note
+from . import db
 
 views = Blueprint("views", __name__)
 
